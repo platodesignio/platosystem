0daf0cb4422e237394884bcf8c0e375e1b5882a2
platodesignio <platonetwork@icloud.com>
2026-02-11 14:26:13 +0900

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient | null = null;
-
-function getPrismaClient(): PrismaClient {
-  if (!prisma) {
-    prisma = new PrismaClient({
-      log:
-        process.env.NODE_ENV === "development"
-          ? ["query", "error", "warn"]
-          : ["error"]
-    });
-  }
-
-  return prisma;
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
 }
 
-export { getPrismaClient as prisma };
+export const prisma =
+  global.prisma ??
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"]
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
